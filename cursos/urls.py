22032d952730ecac72f55d99f5676a2c3f1be4ee
Lucas Farias <lucas_farias6@outlook.com>
2023-11-30 10:19:54 -0300

    Small fix

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,6 +5,5 @@ app_name = 'cursos'
 
 urlpatterns = [
     path('', views.cursos, name="cursos"),
-    path('<str:nome_curso>/<str:nome_modulo>/<str:nome_aula>/', views.aula_detail, name='aula_detail'),
-    path('curso/<int:curso_id>/', views.detalhes_curso, name='detalhes_curso'),
+path('<str:nome_curso>/<str:nome_modulo>/<str:nome_aula>/', views.aula_detail, name='aula_detail'),    path('curso/<int:curso_id>/', views.detalhes_curso, name='detalhes_curso'),
 ]
